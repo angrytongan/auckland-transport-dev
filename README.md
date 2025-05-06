966f2e7d31cb6439bbd8908e82a8b90b9ba57edc
Dean Fogarty <angrytongan@gmail.com>
2025-05-06 18:01:16 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Aucklnd Transport Developer Portal API
+
+Messing about with the [Auckland Transport Developer Portal
+API](https://dev-portal.at.govt.nz).
---
+++ bin/realtime.js
@@ -0,0 +1,43 @@
+const legacyURL = "https://api.at.govt.nz/realtime/legacy/vehiclelocations?tripid&vehicleid";
+
+const field = (f) => {
+    return f ?? '';
+}
+
+const getData = async (url, key) => {
+    const init = {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json",
+            "Ocp-Apim-Subscription-Key": key,
+        },
+    };
+
+    fetch(url, init)
+        .then((resp) => resp.json())
+        .then((data) => {
+            if (data.status === "OK") {
+                console.log("lat,lng,bearing,speed,ts,vehicle_id,vehicle_label,vehicle_plate")
+
+                data.response.entity.forEach((r) => {
+                    const fields = [
+                        field(r.vehicle?.position?.latitude),
+                        field(r.vehicle?.position?.longitude),
+                        field(r.vehicle?.position?.bearing),
+                        field(r.vehicle?.position?.speed),
+                        field(r.vehicle?.timestamp),
+                        field(r.vehicle?.vehicle?.id),
+                        field(r.vehicle?.vehicle?.label),
+                        field(r.vehicle?.vehicle?.license_plate),
+                    ];
+
+                    console.log(fields.join(","));
+                });
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+};
+
+await getData(legacyURL, process.env.APTD_PRIMARY_KEY);
